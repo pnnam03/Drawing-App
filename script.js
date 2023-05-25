a0360a9a27f36cd992a1c88503eac7d1e8781391
y0urs3lf <pnnam0330@gmail.com>
2023-05-25 14:26:28 +0700

    Fixed bugs with selection tool and undo/redo

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ let ctx = canvas.getContext("2d"),
   cancelDragging = false,
   isFilledBack = false,
   dx = 0,
-  dy = 0;
+  dy = 0,
+  tmpsnapshot = null;
 
 // initialize a selection area
 shape = { x: null, y: null, w: null, h: null };
@@ -47,6 +48,7 @@ window.addEventListener("load", () => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   reundoBtn.disabled = true;
+  selectionInterupt();
 });
 
 window.addEventListener("resize", () => {
@@ -59,6 +61,7 @@ window.addEventListener("resize", () => {
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  selectionInterupt();
   if (lastSnapshot != null) ctx.putImageData(lastSnapshot, 0, 0);
 });
 
@@ -108,7 +111,7 @@ const draw = (e) => {
   }
 
   if (!isDrawing) return;
-  ctx.putImageData(snapshot, 0, 0);
+  if (snapshot != null) ctx.putImageData(snapshot, 0, 0);
 
   switch (currentTool) {
     case "brush":
@@ -141,26 +144,38 @@ function selectionInitializer() {
   isDrawing = false;
   cancelDragging = false;
   curSelection = null;
+  tmpsnapshot = null;
   isFilledBack = false;
   shape = { x: null, y: null, w: null, h: null };
   return;
 }
 
 function renderSelection(rect) {
-  if (snapshot != null) tmpCtx.putImageData(snapshot, 0, 0);
+  if (tmpsnapshot != null) tmpCtx.putImageData(tmpsnapshot, 0, 0);
   if (curSelection != null) tmpCtx.putImageData(curSelection, rect.x, rect.y);
   tmpCtx.lineWidth = 1;
   tmpCtx.setLineDash([5]);
   tmpCtx.strokeRect(rect.x, rect.y, rect.w, rect.h);
 }
 
+function selectionInterupt() {
+  // mousedown and not inside selected area
+  // select, drag and drop finished
+  if (curSelection != null) ctx.putImageData(curSelection, shape.x, shape.y);
+  tmpCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // initialize selection variables for next selection
+  selectionInitializer();
+}
+
 const mouseDown = (e) => {
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
   if (currentTool == "selection") {
     if (cancelDragging) return;
     if (!existShape) {
       // not finished selecting area
       isDrawing = true;
-      snapshot = tmpCtx.getImageData(0, 0, canvas.width, canvas.height);
+      tmpsnapshot = tmpCtx.getImageData(0, 0, canvas.width, canvas.height);
       shape.x = e.offsetX;
       shape.y = e.offsetY;
       return;
@@ -179,20 +194,24 @@ const mouseDown = (e) => {
       isDragging = true;
       dx = e.offsetX - shape.x;
       dy = e.offsetY - shape.y;
+      // this solve the misinteraction between Undo/Redo and Selection Tool
+      reundoBtn.disabled = true;
+      undoBtn.disabled = false;
       return;
     }
 
-    // mousedown and not inside selected area
-    // select, drag and drop finished
-    ctx.putImageData(curSelection, shape.x, shape.y);
-    tmpCtx.clearRect(0, 0, canvas.width, canvas.height);
+    selectionInterupt();
+    snapshot = lastSnapshot;
 
-    // initialize selection variables for next selection
-    selectionInitializer();
+    // this solve the misinteraction between Undo/Redo and Selection Tool
+    reundoBtn.disabled = true;
+    undoBtn.disabled = false;
     return;
   }
 
   // current tool is not selection tool
+  selectionInterupt();
+
   isDrawing = true;
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
@@ -207,14 +226,20 @@ const mouseDown = (e) => {
 
   reundoBtn.disabled = true;
   undoBtn.disabled = false;
-  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 };
 
 const mouseUp = () => {
   if (currentTool == "selection") {
     isDragging = false;
     isDrawing = false;
+
     if (!existShape) {
+      // this solved the problem when you drag from different start direction
+      if (shape.w < 0) shape.x += shape.w;
+      if (shape.h < 0) shape.y += shape.h;
+      shape.w = Math.abs(shape.w);
+      shape.h = Math.abs(shape.h);
+
       // the selected area is stored in curSelection
       curSelection = ctx.getImageData(shape.x, shape.y, shape.w, shape.h);
     }
@@ -225,6 +250,7 @@ const mouseUp = () => {
 };
 
 const clearCanvas = () => {
+  selectionInterupt();
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
@@ -251,26 +277,32 @@ toolBtns.forEach((btn) => {
     btn.classList.add("active");
     currentTool = btn.id;
     if (currentTool == "selection") selectionInitializer();
+    selectionInterupt();
   });
 });
 
 function setColor(element) {
   currentColor = element.style.background;
+  selectionInterupt();
 }
 
 colorPicker.addEventListener("input", () => {
+  selectionInterupt();
   currentColor = colorPicker.value;
 });
 
 sizeSlider.addEventListener("input", () => {
+  selectionInterupt();
   currentSize = sizeSlider.value;
 });
 
 fillColor.addEventListener("input", () => {
+  selectionInterupt();
   isFill = true;
 });
 
 undoBtn.addEventListener("click", () => {
+  selectionInterupt();
   curSnapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
   ctx.putImageData(snapshot, 0, 0);
   undoBtn.disabled = true;
@@ -278,12 +310,14 @@ undoBtn.addEventListener("click", () => {
 });
 
 reundoBtn.addEventListener("click", () => {
+  selectionInterupt();
   ctx.putImageData(curSnapshot, 0, 0);
   reundoBtn.disabled = true;
   undoBtn.disabled = false;
 });
 
 saveBtn.addEventListener("click", () => {
+  selectionInterupt();
   console.log("btn clicked");
   const link = document.createElement("a");
   link.download = "myImage.jpg";
